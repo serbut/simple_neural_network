86c1c695108423d5e93c98b45bd5d0f94a35926a
serbut <serbut555@gmail.com>
2017-11-22 16:53:52 +0300

    Some changes

--- application.py
+++ application.py
@@ -10,39 +10,27 @@ FLAGS = None
 
 
 def deepnn(x):
-    """deepnn builds the graph for a deep net for classifying digits.
-    Args:
-      x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-      number of pixels in a standard MNIST image.
-    Returns:
-      A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
-      equal to the logits of classifying the digit into one of 10 classes (the
-      digits 0-9). keep_prob is a scalar placeholder for the probability of
-      dropout.
-    """
-    # Reshape to use within a convolutional neural net.
-    # Last dimension is for "features" - there is only one here, since images are
-    # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+    # Reshape input
     with tf.name_scope('reshape'):
         x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-    # First convolutional layer - maps one grayscale image to 32 feature maps.
+    # First convolutional layer
     with tf.name_scope('conv1'):
         W_conv1 = weight_variable([5, 5, 1, 32])
         b_conv1 = bias_variable([32])
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-    # Pooling layer - downsamples by 2X.
+    # Pooling layer - downsamples by 2X
     with tf.name_scope('pool1'):
         h_pool1 = max_pool_2x2(h_conv1)
 
-    # Second convolutional layer -- maps 32 feature maps to 64.
+    # Second convolutional layer
     with tf.name_scope('conv2'):
         W_conv2 = weight_variable([5, 5, 32, 64])
         b_conv2 = bias_variable([64])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
-    # Second pooling layer.
+    # Second pooling layer
     with tf.name_scope('pool2'):
         h_pool2 = max_pool_2x2(h_conv2)
 
@@ -71,24 +59,20 @@ def deepnn(x):
 
 
 def conv2d(x, W):
-    """conv2d returns a 2d convolution layer with full stride."""
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    """max_pool_2x2 downsamples a feature map by 2X."""
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
 def weight_variable(shape):
-    """weight_variable generates a weight variable of a given shape."""
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    """bias_variable generates a bias variable of a given shape."""
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
@@ -106,11 +90,13 @@ def main(_):
     sess = read_model()
     tf.global_variables_initializer()
 
-    # recognize_random(mnist, sess, y_conv, x, keep_prob)
+    # train(mnist, y_conv, y_, x, keep_prob)
+
+    recognize_random(mnist, sess, y_conv, x, keep_prob)
 
     # test_accuracy(mnist, sess, y_conv, x, y_, keep_prob)
 
-    recognize_user_input(sess, y_conv, x, keep_prob)
+    # recognize_user_input(sess, y_conv, x, keep_prob)
 
 
 def recognize_random(mnist, sess, y_conv, x, keep_prob):
